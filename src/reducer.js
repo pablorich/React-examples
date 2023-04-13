d85adbdf8a22e320dc32b14ed451d5360f3aeb50
Pablo Rivera <pablorich1@hotmail.com>
2023-04-12 21:05:13 -0600

    Initial commit

---
+++ src/reducer.js
@@ -0,0 +1,32 @@
+export const initialState = {
+  stateStatus: "Initial state",
+  todos: [
+    { id: 1, text: "initial1" },
+    { id: 2, text: "intiial2" }
+  ]
+};
+
+export const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TODO":
+      if (state.stateStatus == "Initial state") {
+        return {
+          stateStatus: "modified",
+          todos: [action.payload]
+        };
+      }
+      return {
+        ...state,
+        todos: [...state.todos, action.payload]
+      };
+
+    case "DELETE_TODO":
+      return {
+        ...state,
+        todos: [...state.todos.filter((item) => item.id !== action.payload)]
+      };
+
+    default:
+      return state;
+  }
+};
