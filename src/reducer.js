5641f0fe6335d0e76cb464954972fbe460de4096
Pablo Rivera <pablorich1@hotmail.com>
2023-04-13 03:11:06 +0000

    Update reducer.js

--- src/reducer.js
+++ src/reducer.js
@@ -2,28 +2,28 @@ export const initialState = {
   stateStatus: "Initial state",
   todos: [
     { id: 1, text: "initial1" },
-    { id: 2, text: "intiial2" }
-  ]
+    { id: 2, text: "intiial2" },
+  ],
 };
 
 export const reducer = (state, action) => {
   switch (action.type) {
     case "ADD_TODO":
-      if (state.stateStatus == "Initial state") {
+      if (state.stateStatus === "Initial state") {
         return {
           stateStatus: "modified",
-          todos: [action.payload]
+          todos: [action.payload],
         };
       }
       return {
         ...state,
-        todos: [...state.todos, action.payload]
+        todos: [...state.todos, action.payload],
       };
 
     case "DELETE_TODO":
       return {
         ...state,
-        todos: [...state.todos.filter((item) => item.id !== action.payload)]
+        todos: [...state.todos.filter((item) => item.id !== action.payload)],
       };
 
     default:
